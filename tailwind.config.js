ae148f731233bcceb89176901bdb5ecd27e86550
Esteban Fortini <88391276+EstebanFortini@users.noreply.github.com>
2025-11-06 18:53:46 -0300

    Update tailwind.config.js - dark class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',             // <- agrega esta línea
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
